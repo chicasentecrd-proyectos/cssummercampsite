19c6b941f81283e99491cae19ffc1ff7aa7f8c30
Arris Moise <mikaelmoise00@gmail.com>
2021-07-17 17:33:02 -0400

    added sections and background image

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+
+    <section class="min-h-screen text-center p-32 mb-20" style="background-image: url('https://i.ibb.co/y8MrnMB/girls.png'); background-size: cover">
+      <h1 class="font-bold text-white text-6xl mt-44" > Computer Science <br> Summer Camp For Girls </h1>
+
+
+
+
+    </section>
+
+    <!-- About Us , Our Donors, Contact Us Section -->
+
+    <section class="text-center">
+      <div class="space-y-4">
+          <div class="flow-root ...">
+            <div class="my-4 ...">
+
+            <div class="bg-indigo-600 rounded-full shadow-xl col-span-1">
+         <div class="">
+
+           <h1 class="text-white">About Us</h1>
+
+         </div>
+           </div>
+        </div>
+
+          </div>
+          <div class="flow-root ...">
+            <div class="my-4 ..."> Our Donors</div>
+          </div>
+          <div class="flow-root ...">
+            <div class="my-4 ..."> Contact Us</div>
+          </div>
+      </div>
+    </section>
+
+    <!-- Girls Work Section -->
+
+    <section>
+    </section>
+
+
+  </div>
+</template>
+<script>
+
+
+
+</script>
